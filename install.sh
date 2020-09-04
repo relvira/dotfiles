ef476304330a89673f8632e9b614b24ae42ffecf
Rafael Elvira <relvira@slack-corp.com>
2020-09-04 00:45:12 -0700

    modified dummy installer

--- install.sh
+++ install.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+bashrc_path=~/.bashrc
+if test -f "$bashrc_path"; then
+  mv $bashrc_path ~/.bashrc.old
+fi
+
+tmux_conf=~/.tmux.conf
+if test -f "$tmux_conf"; then
+  mv $tmux_conf ~/.tmux.conf.old
+fi
+
 # Install TPM (Tmux Plugin Manager)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Install vim package manager
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+
 # just ln -s things
-ln -s ~/src/bashrc ~/.bashrc
-ln -s ~/src/tmux ~/.tmux
-ln -s ~/src/tmux.conf ~/.tmux.conf
-ln -s ~/src/nvim ~/.config/nvim
+ln -s ~/src/dotfiles/bashrc ~/.bashrc
+ln -s ~/src/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/src/dotfiles/init.vim ~/.config/nvim/init.vim
 
